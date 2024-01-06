28aa1454439650fb0104be77bf3ccd163a39f671
Aleksander Ivanov <62203063+japsap@users.noreply.github.com>
2024-01-07 00:43:14 +0200

    database updated

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function db(){
     try{
-        await mongoose.connect('mongodb://127.0.0.1:27017/Retrovate-online-store')
+        await mongoose.connect(process.env.MONGODB_URL)
         console.log('Database connected!');
     } catch(err){
         console.log(err)
