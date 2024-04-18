2bdf6f1017eca02ce41c42ead44de9475210159e
Aleksander Ivanov <62203063+japsap@users.noreply.github.com>
2024-04-18 11:08:42 +0300

    opsi

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function db() {
   try {
-    await mongoose.connect(process.env.MONGODB_LOCAL_URI);
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("Database connected!");
   } catch (err) {
     console.log(err);
